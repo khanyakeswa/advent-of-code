7a7c82b564f38243fc2756d977cad580a3e866ea
Khanya <khanya.keswa@gmail.com>
2023-11-04 22:55:38 -0400

    day 1 complete!

--- 2022/day1/index.ts
+++ 2022/day1/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+const rawCalorieList = await Bun.file('testsample.txt').text()
+
+// console.log(rawCalorieList);
+
+const calorieCounter = () => {
+  const calorieList = rawCalorieList.split('\n')
+  const processedCalorieList: number[][] = []
+  let elfCals: number[] = []
+  for (let i = 0; i < calorieList.length; i++) {
+      if (calorieList[i]) {
+        elfCals.push(Number(calorieList[i]))
+    } else {
+        processedCalorieList.push(elfCals)
+        elfCals = []
+    }
+  }
+  
+  // [[1, 2, 3], [1], [1, 2]] => [1, 2, 3]
+  const calorieSums = processedCalorieList.map((elfCals) => {
+      const calorieSum = elfCals.reduce((sum, currentCalorie) => sum + currentCalorie, 0)
+      
+      return calorieSum;
+    })
+    console.log(calorieSums.sort((a, b) => b - a)[0])
+}
+
+calorieCounter()
+
+// reduce: [1, 2, 3] => 6
+// map: [1, 2, 3] => [11, 22, 33] done!
+// filter: [1, 2, 3, 4, 5] => [2, 4]
