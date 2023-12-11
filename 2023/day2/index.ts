a2d18979ebe0e7f37f97e8d0f5f65dac8b4d61c6
Khanya <khanya.keswa@gmail.com>
2023-12-11 14:33:43 -0500

    day2 progressday2 progressday2 progress

--- 2023/day2/index.ts
+++ 2023/day2/index.ts
@@ -4,8 +4,10 @@ const cubeCounter = () => {
     const games: string[] = rawData.split('\n')
     const gamesCollections = []
     let gameData = {gameId: 0, maxRed: 0, maxGreen: 0, maxBlue: 0}
-    
-    console.log(games)
+    for (let i = 0; i < games.length; i++) {
+        gameData.gameId = parseInt(games[i].slice((games[i].indexOf('e') + 1), (games[i].indexOf(':'))))
+    }
+    console.log(gameData.gameId)
 }
 
 cubeCounter()
